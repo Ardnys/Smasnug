21da9cfa5f60298240839bf57fb86d356e909664
Ardnys <semresavas35@gmail.com>
2023-11-18 20:51:50 +0300

    add username and password validation

--- src/Login.jsx
+++ src/Login.jsx
@@ -5,8 +5,69 @@ import viteLogo from '/vite.svg';
 import './App.css';
 
 function Login() {
+    // form validation is taken from
+    // https://stackoverflow.com/questions/41296668/how-do-i-add-validation-to-the-form-in-my-react-component
+
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [usernameError, setUsernameError] = useState('');
+    const [passwordError, setPasswordError] = useState('');
+
+    const validateUsername = () => {
+        let usernameValid = true;
+
+        if (username === '') {
+            usernameValid = false;
+            setUsernameError('*username cannot be empty');
+        } else if (typeof username !== 'undefined') {
+            if (!username.match(/^[a-zA-Z0-9]+$/)) {
+                usernameValid = false;
+                setUsernameError(
+                    '*only letters and numbers are allowed in username'
+                );
+            }
+        }
+        return usernameValid;
+    };
+
+    const validatePassword = () => {
+        let passwordValid = true;
+        if (password === '') {
+            passwordValid = false;
+            setPasswordError('*password cannot be empty');
+        } else if (typeof password !== 'undefined') {
+            if (
+                password.match(
+                    '^(.{0,7}|[^0-9]*|[^A-Z]*|[^a-z]*|[a-zA-Z0-9]*)$'
+                )
+            ) {
+                // regex taken from Matt Timmermans. I think I actually understand this regex:
+                // https://stackoverflow.com/questions/19605150/regex-for-password-must-contain-at-least-eight-characters-at-least-one-number-a
+                console.error(password);
+                passwordValid = false;
+                setPasswordError(
+                    '*password must contain minimum 8 characters, ' +
+                        'at least one letter, one number and one special character.'
+                );
+            }
+        }
+        return passwordValid;
+    };
+
+    let navigate = useNavigate();
+
+    const validate = () => {
+        setUsernameError('');
+        setPasswordError('');
+        let iuv = validateUsername();
+        let ipv = validatePassword();
+        if (iuv && ipv) {
+            let path = 'login';
+            navigate(path);
+        } else {
+            // console.error('try again');
+        }
+    };
 
     const inputStyle = {
         fontSize: '32px',
@@ -16,13 +77,6 @@ function Login() {
         fontSize: '64px',
         color: '#646cffaa',
     };
-
-    let navigate = useNavigate();
-    const routeChange = () => {
-        let path = 'login';
-        navigate(path);
-    };
-
     return (
         <>
             <h1 style={titleStyle}>Smasnug Galoxy Phon Book</h1>
@@ -32,7 +86,7 @@ function Login() {
                     className="logo"
                     alt="smasnug. it's misspelled samsung"
                 />
-                <form className="fields">
+                <div className="fields">
                     <h2>Username</h2>
                     <input
                         type="text"
@@ -40,6 +94,7 @@ function Login() {
                         style={inputStyle}
                         onChange={(event) => setUsername(event.target.value)}
                     />
+                    <span className="error">{usernameError}</span>
                     <h2>Password</h2>
 
                     <input
@@ -48,14 +103,17 @@ function Login() {
                         style={inputStyle}
                         onChange={(event) => setPassword(event.target.value)}
                     />
-                </form>
-                <button
-                    type="button"
-                    className="loginButton"
-                    onClick={routeChange}
-                >
-                    Login
-                </button>
+                    <span className="error">{passwordError}</span>
+                    <button
+                        type="button"
+                        className="loginButton"
+                        id="submit"
+                        value="Submit"
+                        onClick={validate}
+                    >
+                        Login
+                    </button>
+                </div>
             </div>
         </>
     );
